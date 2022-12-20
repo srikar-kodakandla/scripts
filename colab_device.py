a0490a1ee5e62f1415384f69c1b45ca988722be4
srikar kodakandla <kodakandlasrikar99@gmail.com>
2022-12-20 23:16:23 +0530

    Update colab_device.py

--- colab_device.py
+++ colab_device.py
@@ -5,7 +5,7 @@ try:
   #!pip install cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl
   #!pip install torch_tb_profiler
   import subprocess
-  subprocess.check_call([sys.executable, "-m", "pip", "install", torch_tb_profiler cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl])
+  subprocess.check_call([sys.executable, "-m", "pip", "install", "torch_tb_profiler cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl"])
   import torch_xla.distributed.parallel_loader as pl
   import torch_xla
   import torch_xla.core.xla_model as xm
