20d1c75acffff744c416ec465f2256375902f530
srikar kodakandla <kodakandlasrikar99@gmail.com>
2022-12-20 23:21:51 +0530

    Update colab_device.py

--- colab_device.py
+++ colab_device.py
@@ -1,19 +1,22 @@
 try:
   import os 
-  #os.system("pip install cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl")
-  #os.system("pip install torch_tb_profiler")
+  os.system("pip install cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl")
+  os.system("pip install torch_tb_profiler")
   #!pip install cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl
   #!pip install torch_tb_profiler
   import subprocess
-  subprocess.check_call([sys.executable, "-m", "pip", "install", "torch_tb_profiler cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl"])
+  import sys
+  #subprocess.check_call(["pip", "install", "torch_tb_profiler cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl"])
+  print("If you run this for the first time ,Restart the Colab Instance and run this cell again")
   import torch_xla.distributed.parallel_loader as pl
   import torch_xla
   import torch_xla.core.xla_model as xm
   device = xm.xla_device()
 except:
-  print("TPU not found,Using Cuda ")
   import torch
   if torch.cuda.is_available():
+    print("TPU not found,GPU found,Using Cuda ")
     device="cuda"
   else:
+    print("TPU and GPU not found,Using CPU ")
     device="cpu"
