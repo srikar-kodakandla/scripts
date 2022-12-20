96b43b61f417f5fff0f6726ca8bf8503a4671735
srikar kodakandla <kodakandlasrikar99@gmail.com>
2022-12-20 23:00:53 +0530

    Update tpu_colab.py

--- tpu_colab.py
+++ tpu_colab.py
@@ -1,7 +1,12 @@
-import os 
-os.system("pip install cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl")
-os.system("pip install torch_tb_profiler")
-import torch_xla.distributed.parallel_loader as pl
-import torch_xla
-import torch_xla.core.xla_model as xm
-device = xm.xla_device()
+try:
+  import os 
+  os.system("pip install cloud-tpu-client==0.10 torch==1.13.0 https://storage.googleapis.com/tpu-pytorch/wheels/colab/torch_xla-1.13-cp38-cp38-linux_x86_64.whl")
+  os.system("pip install torch_tb_profiler")
+  !pip install torch
+  import torch_xla.distributed.parallel_loader as pl
+  import torch_xla
+  import torch_xla.core.xla_model as xm
+  device = xm.xla_device()
+except:
+  print("TPU not found,Using Cuda ")
+  device="cuda"
